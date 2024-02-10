25441490940bb019189a43a8d99b94bca3e47f34
PhongHNg <86726369+phonghng@users.noreply.github.com>
2024-02-10 04:31:03 +0000

    Cập nhật check_news

--- Mirmor/scripts/settings.js
+++ Mirmor/scripts/settings.js
@@ -26,7 +26,7 @@ const HABITS = {
                 },
                 check_news: {
                     type: "habit_check",
-                    name: "Kiểm tra tin tức trên <a href='https://www.youtube.com/' target='_blank'>YouTube</a>, <a href='https://www.msn.com/vi-vn/feed' target='_blank'>Microsoft Start</a>, <a href='https://news.google.com/' target='_blank'>Google News</a>",
+                    name: "Kiểm tra tin tức trên <a href='https://www.youtube.com/feed/subscriptions' target='_blank'>YouTube</a>, <a href='https://news.google.com/home?hl=vi&gl=VN&ceid=VN:vi' target='_blank'>Google News</a>, <a href='https://www.msn.com/en-us/feed' target='_blank'>Microsoft Start</a>",
                     required: true,
                     point: 75
                 },
