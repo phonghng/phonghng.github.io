1e9104ddb9a1ac27c5ef62c5f1835d2affc9c4d2
PhongHNg <86726369+phonghng@users.noreply.github.com>
2024-06-13 01:11:13 +0000

    Cập nhật bookmark; cho phép folder trong folder

--- scripts/PPUPS.js
+++ scripts/PPUPS.js
@@ -1,70 +1,72 @@
+let folder_count = 0;
+
 function process_item(item, item_list_element) {
+    let ADOMs = [];
+
     switch (item.type) {
         case "widget_image": {
-            item_list_element.appendChild(
-                PPPL_JS.ADOM([
-                    "div",
+            ADOMs.push([
+                "div",
+                {
+                    "class": "item widget",
+                    "id": item.id,
+                    "data-link": item.image_source,
+                    "title": item.image_source
+                },
+                {
+                    "onclick": () => location.href = item.image_source
+                },
+                [[
+                    "img",
                     {
-                        "class": "item widget",
-                        "id": item.id,
-                        "data-link": item.image_source,
-                        "title": item.image_source
+                        "src": item.image_source,
+                        "style": "--base_color: " + item.image_base_color + ";"
                     },
-                    {
-                        "onclick": () => location.href = item.image_source
-                    },
-                    [[
-                        "img",
-                        {
-                            "src": item.image_source,
-                            "style": "--base_color: " + item.image_base_color + ";"
-                        },
-                        null,
-                        null,
-                        null
-                    ]],
+                    null,
+                    null,
                     null
-                ], document)
-            );
+                ]],
+                null
+            ]);
             break;
         }
 
         case "widget_iframe": {
-            item_list_element.appendChild(
-                PPPL_JS.ADOM([
-                    "div",
+            ADOMs.push([
+                "div",
+                {
+                    "class": "item widget",
+                    "id": item.id,
+                    "data-link": item.iframe_url,
+                    "title": item.iframe_url
+                },
+                {
+                    "onclick": () => location.href = item.iframe_url
+                },
+                [[
+                    "iframe",
                     {
-                        "class": "item widget",
-                        "id": item.id,
-                        "data-link": item.iframe_url,
-                        "title": item.iframe_url
+                        "src": item.iframe_url
                     },
-                    {
-                        "onclick": () => location.href = item.iframe_url
-                    },
-                    [[
-                        "iframe",
-                        {
-                            "src": item.iframe_url
-                        },
-                        null,
-                        null,
-                        null
-                    ]],
+                    null,
+                    null,
                     null
-                ], document)
-            );
+                ]],
+                null
+            ]);
             break;
         }
 
         case "folder": {
-            let children_ADOM = [];
+            let folder_id = "folder_" + folder_count++;
 
-            children_ADOM.push([
+            ADOMs.push([
                 "div",
                 {
-                    "class": "item button",
-                    "title": item.name
+                    "class": "item folder_button",
+                    "title": item.name,
+                    "data-opened": item.opened.toString(),
+                    "style": "--folder_color: " + item.color + ";"
                 },
                 null,
                 [[
@@ -77,68 +79,79 @@ function process_item(item, item_list_element) {
                     null,
                     null
                 ]],
-                null
+                button => {
+                    button.onclick = () => {
+                        let folder = document.querySelector("#" + folder_id);
+                        let new_state = folder.dataset.opened == "false";
+                        folder.dataset.opened = new_state;
+                        button.dataset.opened = new_state;
+                        mansory.layout();
+                    };
+                }
             ]);
 
-            item.items.forEach(folder_item => {
-                let icons = [];
+            let children_ADOM = [];
+            item.items
+                .filter(folder_item => folder_item.type == "folder" || folder_item.type == "item")
+                .forEach(folder_item => {
+                    if (folder_item.type == "folder") {
+                        process_item(folder_item).forEach(ADOM => children_ADOM.push(ADOM));
+                        return;
+                    }
 
-                icons.push([
-                    "img",
-                    {
-                        "src": folder_item.icon_source,
-                        "style": "--base_color: " + folder_item.icon_base_color + ";"
-                    },
-                    null,
-                    null,
-                    null
-                ]);
+                    let icons = [];
 
-                if (folder_item.sub_icon_source)
                     icons.push([
                         "img",
                         {
-                            "src": folder_item.sub_icon_source,
-                            "style": "--base_color: " + folder_item.sub_icon_base_color + ";"
+                            "src": folder_item.icon_source,
+                            "style": "--base_color: " + folder_item.icon_base_color + ";"
                         },
                         null,
                         null,
                         null
                     ]);
 
-                children_ADOM.push([
-                    "div",
-                    {
-                        "class": "item",
-                        "data-link": folder_item.link,
-                        "title": folder_item.link
-                    },
-                    {
-                        "onclick": () => location.href = folder_item.link
-                    },
-                    icons,
-                    null
-                ]);
-            });
+                    if (folder_item.sub_icon_source)
+                        icons.push([
+                            "img",
+                            {
+                                "src": folder_item.sub_icon_source,
+                                "style": "--base_color: " + folder_item.sub_icon_base_color + ";"
+                            },
+                            null,
+                            null,
+                            null
+                        ]);
 
-            item_list_element.appendChild(
-                PPPL_JS.ADOM([
-                    "div",
-                    {
-                        "class": "folder",
-                        "data-opened": item.opened.toString(),
-                        "style": "--color: " + item.color + ";"
-                    },
-                    null,
-                    children_ADOM,
-                    folder => {
-                        folder.querySelector(".item.button").onclick = () => {
-                            folder.dataset.opened = folder.dataset.opened == "false";
-                            mansory.layout();
-                        };
-                    }
-                ], document)
-            );
+                    children_ADOM.push([
+                        "div",
+                        {
+                            "class": "item",
+                            "data-link": folder_item.link,
+                            "title": folder_item.link
+                        },
+                        {
+                            "onclick": () => location.href = folder_item.link
+                        },
+                        icons,
+                        null
+                    ]);
+                });
+
+            ADOMs.push([
+                "div",
+                {
+                    "id": folder_id,
+                    "class": "folder",
+                    "data-opened": item.opened.toString(),
+                    "style": "--color: " + item.color + ";"
+                },
+                null,
+                children_ADOM,
+                null
+            ]);
+
             break;
         }
 
@@ -168,24 +181,28 @@ function process_item(item, item_list_element) {
                     null
                 ]);
 
-            item_list_element.appendChild(
-                PPPL_JS.ADOM([
-                    "div",
-                    {
-                        "class": "item",
-                        "data-link": item.link,
-                        "title": item.link
-                    },
-                    {
-                        "onclick": () => location.href = item.link
-                    },
-                    icons,
-                    null
-                ], document)
-            );
+            ADOMs.push([
+                "div",
+                {
+                    "class": "item",
+                    "data-link": item.link,
+                    "title": item.link
+                },
+                {
+                    "onclick": () => location.href = item.link
+                },
+                icons,
+                null
+            ]);
             break;
         }
     };
+
+    if (item_list_element)
+        ADOMs.forEach(ADOM =>
+            item_list_element.appendChild(PPPL_JS.ADOM(ADOM, document))
+        );
+    return ADOMs;
 }
 
 let item_list_element = document.querySelector("#item_list");
@@ -198,4 +215,3 @@ let mansory = new Masonry("#item_list", {
     gutter: 20,
     fitWidth: true
 });
-
